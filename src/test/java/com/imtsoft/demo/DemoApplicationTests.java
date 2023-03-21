c55176c1b1d958591863057bd90a1bd86851ae0f
duongquocanh1148 <duongquocanh1148@gmail.com>
2023-03-21 09:53:45 +0700

    first commit

---
+++ src/test/java/com/imtsoft/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imtsoft.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
